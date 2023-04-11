9892d247eaa2e87a6fead2c4d05c70c5e7657b94
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2023-04-11 14:12:27 -0300

    chore: add rings animated

--- src/shared/common/components/Splash/index.tsx
+++ src/shared/common/components/Splash/index.tsx
@@ -1,19 +1,19 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-expressions */
 import React, { useCallback, useEffect } from 'react'
-import Animated, {
-  Extrapolate,
-  interpolate,
+import {
   runOnJS,
-  useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated'
-import { Text } from '../Text'
 
 import { RootSplashParamsList } from '@shared/routes/splash.routes'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { useNavigation } from '@react-navigation/native'
+import { LogoImage } from '@modules/authentication/screens/Welcome/styles'
 import { Container } from './styles'
+
+import Ring from './ring'
 
 type SplashScreenProps = NativeStackNavigationProp<
   RootSplashParamsList,
@@ -24,40 +24,7 @@ const Splash = (): JSX.Element => {
   const { navigate } = useNavigation<SplashScreenProps>()
   const splashAnimation = useSharedValue(0)
 
-  const brandStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
-      transform: [
-        {
-          translateX: interpolate(
-            splashAnimation.value,
-            [0, 50],
-            [0, -50],
-            Extrapolate.CLAMP,
-          ),
-        },
-      ],
-    }
-  })
-
-  const logoStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
-      transform: [
-        {
-          translateX: interpolate(
-            splashAnimation.value,
-            [0, 50],
-            [-50, 0],
-            Extrapolate.CLAMP,
-          ),
-        },
-      ],
-    }
-  })
-
   const startApp = useCallback(() => {
-    // navigation.dispatch(StackActions.replace('Home'));
     navigate('App')
   }, [navigate])
 
@@ -66,7 +33,7 @@ const Splash = (): JSX.Element => {
 
     splashAnimation.value = withTiming(50, { duration: 1000 }, () => {
       if (mounted) {
-        ;('worklet')
+        ('worklet')
         runOnJS(startApp)()
       }
     })
@@ -78,14 +45,16 @@ const Splash = (): JSX.Element => {
 
   return (
     <Container>
-      {/* <Animated.View style={[brandStyle, { position: 'absolute' }]}>
-        <BrandSvg width={117} height={113} />
-      </Animated.View>
+      <Ring delay={0} />
+      <Ring delay={500} />
+      <Ring delay={1000} />
+      <Ring delay={1500} />
+      <LogoImage
+        source={{
+          uri: 'https://codeleap.co.uk/static/838fbf40bc335c2ffd2ea5ee19ed3baa/f6c84/codeleap_logo_white.webp',
+        }}
+      />
 
-      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
-        <LogoSvg width={400} height={71} />
-      </Animated.View> */}
-      <Text>Splash screen</Text>
     </Container>
   )
 }
